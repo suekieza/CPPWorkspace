d1c5a96a339ceb679e7e765352cd49bdb180f6fe
Sue Sauer <suesauer@hotmail.com>
2020-02-02 10:16:30 +0200

    Favorite number

---
+++ Section4/FirstProgram/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+	
+	int fav_num;
+	std::cout << "Type Favorite number between 1 and 100: ";
+	std::cin >> fav_num;
+	std::cout << "Amazing, thats my Favorite number too" << std::endl;
+	return 0;
+}
