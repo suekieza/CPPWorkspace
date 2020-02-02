fa962048a15b9d9b4ad286e27a3703d877511d5e
Sue Sauer <suesauer@hotmail.com>
2020-02-02 10:58:38 +0200

    Completed Section 4

--- Section4/FirstProgram/main.cpp
+++ Section4/FirstProgram/main.cpp
@@ -5,6 +5,8 @@ int main(){
 	int fav_num;
 	std::cout << "Type Favorite number between 1 and 100: ";
 	std::cin >> fav_num;
-	std::cout << "Amazing, thats my Favorite number too" << std::endl;
+	std::cout << "Amazing, "; 
+	std::cout << "thats my favorite number too!" << std::endl;
+	std::cout << "No really, " << fav_num << " is my favorite number! :D" << std::endl;
 	return 0;
 }
